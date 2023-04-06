ad4c41ecd4fc90ff5724fb02b4dac732e409404e
Enes Ilkbay <enes.ilkbay254@gmail.com>
2023-04-06 17:43:47 +0200

    sd

---
+++ src/main/java/de/vikz/wumtbackend/WumtBackendApplication.java
@@ -0,0 +1,13 @@
+package de.vikz.wumtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WumtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WumtBackendApplication.class, args);
+	}
+
+}
